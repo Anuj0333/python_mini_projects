75b832599c97b9751118f72822bce92863682c73
anuj0333 <anuj.gupta.0812.vns@gmail.com>
2023-08-28 22:45:42 +0530

    upside down

---
+++ turtle_project/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+tim.left(angle)
--- turtle_project/turle_shapes.py
+++ turtle_project/turle_shapes.py
@@ -3,7 +3,7 @@ import turtle as t
 import random
 import turtle 
 tim=t.Turtle()
-color=["chocolate1","brown2","azure2"]
+color=["chocolate1","brown2","azure4","lightCoral","khaki4"]
 s=Screen()
 def draw_shapes(num_sides):
         
@@ -11,7 +11,8 @@ def draw_shapes(num_sides):
             
         angle =360/ num_sides
         tim.forward(100)
-        tim.right(angle)
+        tim.left(angle)
+        # tim.circle(angle)
 for shape_side_n in range(3,11):
      tim.color(random.choice(color))
      draw_shapes(shape_side_n)
