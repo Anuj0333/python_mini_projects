19b8b72a192c6cce887bd21c63b6d525e9c54838
anuj0333 <anuj.gupta.0812.vns@gmail.com>
2023-09-10 22:12:50 +0530

    updates in game

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -34,13 +34,17 @@ while game_is_on:
 
     #Detect collision with wall.
     if snake.head.xcor() > 345 or snake.head.xcor() < -345 or snake.head.ycor() > 345 or snake.head.ycor() < -345:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+        # game_is_on = False
+        # scoreboard.game_over()
 
     #Detect collision with tail.
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset( )
+            snake.reset()
+            # game_is_on = False
+            # scoreboard.game_over()
 
 screen.exitonclick()
--- Snake_game/scoreboard.py
+++ Snake_game/scoreboard.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        self.high_score=0
         self.color("white")
         self.penup()
         self.goto(0,340)
@@ -12,20 +13,24 @@ class Scoreboard(Turtle):
 
 
     def update_scoreboard(self):
-        self.write (f"Score:{self.score}",False,align="center",font=("Arial",24,"normal"))
+        self.clear()
+        self.write (f"Score:{self.score}  High Score:{self.high_score}",False,align="center",font=("Arial",24,"normal"))
 
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score=  self.score
+        self.score=0
+        self.update_scoreboard()
 
 
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write ("GAME OVER",False,align="center",font=("Arial",24,"normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write ("GAME OVER",False,align="center",font=("Arial",24,"normal"))
         
 
     def increase_score(self):
         self.score+=1
-        self.clear()
         self.update_scoreboard()
 
 
