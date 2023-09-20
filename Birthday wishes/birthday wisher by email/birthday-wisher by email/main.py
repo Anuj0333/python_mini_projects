9ea63032e913a116e0b185885858c9477b7cf6b4
anuj0333 <anuj.gupta.0812.vns@gmail.com>
2023-09-20 22:08:02 +0530

    updated

--- Birthday wishes/birthday wisher by email/birthday-wisher by email/main.py
+++ Birthday wishes/birthday wisher by email/birthday-wisher by email/main.py
@@ -11,8 +11,7 @@ password="gqqtssunqymavwrp"
 today = (dt.datetime.now().month, dt.datetime.now().day)
 
 # HINT 2: Use pandas to read the birthdays.csv
-data=pd.read_csv("C:/Users/ASUS/OneDrive/Documents/anuj_work/python_mini_projects/Birthday wishes/Birthday+Wisher+(Day+32)+start/birthday-wisher-normal-start/birthdays.csv")
-
+data=pd.read_csv("C:/Users/ASUS/OneDrive/Documents/anuj_work/python_mini_projects/Birthday wishes/birthday wisher by email/birthday-wisher by email/birthdays.csv")
 
 birthdays_dict = {(data_row["month"], data_row["day"]):data_row for (index, data_row) in data.iterrows()}
 
@@ -20,7 +19,7 @@ birthdays_dict = {(data_row["month"], data_row["day"]):data_row for (index, data
 #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
 if today in birthdays_dict:
     birthday_person=birthdays_dict[today]
-    file_path=f"C:/Users/ASUS/OneDrive/Documents/anuj_work/python_mini_projects/Birthday wishes/Birthday+Wisher+(Day+32)+start/birthday-wisher-normal-start/letter_templates/letter_{random.randint(1,3)}.txt"
+    file_path=f"C:/Users/ASUS/OneDrive/Documents/anuj_work/python_mini_projects/Birthday wishes/birthday wisher by email/birthday-wisher by email/letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents=letter_file.read()
         contents=contents.replace("[NAME]",birthday_person["name"])
